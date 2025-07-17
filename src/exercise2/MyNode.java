75b38f433d66b929bdb086bf3aeff333d29833b7
Aryan Bhanot <Aryan.Bhanot@edu.sait.ca>
2025-07-17 00:12:06 -0600

    Update MyNode.java
    predecessor and successor made

--- src/exercise2/MyNode.java
+++ src/exercise2/MyNode.java
@@ -7,4 +7,33 @@ public class MyNode<E>
 	private MyNode<E> next;
 	private MyNode<E> prev;
 
+	public MyNode(E element) {
+        this.element = element;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public E getElement() {
+        return element;
+    }
+
+    public void setElement(E element) {
+        this.element = element;
+    }
+
+    public MyNode<E> getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode<E> next) {
+        this.next = next;
+    }
+
+    public MyNode<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(MyNode<E> prev) {
+        this.prev = prev;
+    }
 }
