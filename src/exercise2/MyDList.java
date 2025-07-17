ec3e76483c95627a39eb4c1f93b75bbd79b9a3e5
Aryan Bhanot <Aryan.Bhanot@edu.sait.ca>
2025-07-17 00:13:15 -0600

    Update MyDList.java
    making required methods

--- src/exercise2/MyDList.java
+++ src/exercise2/MyDList.java
@@ -7,4 +7,22 @@ public class MyDList<E>
 	private MyNode<E> tail;
 	private int size;
 
+	public MyDList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public MyNode<E> getHead() {
+        return head;
+    }
+
+    public void addFirst(E item) {
+        MyNode<E> newNode = new MyNode<>(item);
+        newNode.setNext(head);
+        if (head != null) head.setPrev(newNode);
+        head = newNode;
+        if (tail == null) tail = newNode;
+        size++;
+    }
 }
