a44c2ad97e25729a159fe94b3047d76041022ae7
Aryan Bhanot <Aryan.Bhanot@edu.sait.ca>
2025-07-16 23:30:51 -0600

    Exercise 1 - c1
    first 3 methods and constructors created

--- src/exercise1/MyAList.java
+++ src/exercise1/MyAList.java
@@ -7,6 +7,21 @@ public class MyAList<E> implements Iterable<E>
 {
 	private ArrayList<E> list;
 
+    public MyAList() {
+        list = new ArrayList<>();
+    }
+
+    public void add(E item) {
+        list.add(item);
+    }
+
+    public void addAll(MyAList<E> items) {
+        for (E item : items) {
+            list.add(item);
+        }
+    }
+
+
 	@Override
 	public Iterator<E> iterator()
 	{
